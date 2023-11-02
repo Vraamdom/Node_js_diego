3c24ea05e420d70503501d24877f86ae58f62fe0
Vraamdom <135671271+Vraamdom@users.noreply.github.com>
2023-11-02 09:30:50 -0500

    server.js

--- api2/models/server.js
+++ api2/models/server.js
@@ -30,41 +30,7 @@ listen(){
    )
 }
 routes(){
-    //Listar todos los datos
-    this.app.get('/',(req, res)=>{
-        res.json({
-            msg: usuarios
-        })
-    })
-
-    // Consultar un dato
-    this.app.get('q',(req, res)=>{
-        const {CorreoElectronico} = req.query
-
-        let resultado = ''
-
-        //Buscar si el documento recibido existe en el array aprendices
-
-        resultado = usuarios.find(usuarios => usuarios.CorreoElectronico === CorreoElectronico)
-
-        res.json({
-            msg:resultado
-        })
-    })
-
-    this.app.post('/', (req,res) =>{
-        const {NombreCompleto,CorreoElectronico,Contraseña,ConfirmarContraseña} = req.query
-        usuarios.push({
-            "NombreCompleto" :NombreCompleto,
-            "CorreoElectronico" : CorreoElectronico,
-            "Contraseña": Contraseña,
-            "ConfirmarContraseña":ConfirmarContraseña
-        })
-
-        res.json({
-            msg: usuarios
-        })
-    })
+  
 }
 }
 module.exports ={Server}//exportacion de la clase.
